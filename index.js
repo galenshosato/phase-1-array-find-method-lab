81f090cd1e97d2144d245e89f42b6fc100e9c575
Galen Sho Sato <galen.sato@gmail.com>
2023-01-13 15:36:05 -0500

    Completed Lab

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // code your solution here
+
+  function superbowlWin(array) {
+    let winArray = array.find((element) => element.result === "W")
+    if (winArray === undefined) {
+        return undefined
+    }
+    else {
+        return winArray.year
+    }
+
+  
+}
+
+
